6e0cbde1755439c080d9a419be72bb8240075641
ProgKenhy <sashok20053@gmail.com>
2025-09-20 10:54:58 +0200

    deleted from attributes

--- src/tasks/schemas.py
+++ src/tasks/schemas.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from pydantic import BaseModel, Field, ConfigDict
+from pydantic import BaseModel, Field
 
 
 class TaskInfo(BaseModel):
@@ -8,27 +8,22 @@ class TaskInfo(BaseModel):
     assignee: str = Field(description="Исполнитель задачи")
     task_name: str = Field(max_length=255, description="Название задачи")
     created_at: datetime = Field(description="Дата и время создания задачи")
-    model_config = ConfigDict(from_attributes=True)
 
 
 class TaskCreate(BaseModel):
     assignee: str = Field(description="Исполнитель задачи")
     task_name: str = Field(max_length=255, description="Название задачи")
-    model_config = ConfigDict(from_attributes=True)
 
 
 # class TaskAssigneeGet(BaseModel):
 #     assignee: str = Field(description="Исполнитель задачи")
 #     timeout: int = Field(gt=0, description="Время ожидания задачи")
-#     model_config = ConfigDict(from_attributes=True)
 
 class TaskCreateResponse(BaseModel):
     id: str = Field(description="ID задачи")
-    model_config = ConfigDict(from_attributes=True)
 
 
 class TaskInfoResponse(BaseModel):
     assignee: str = Field(description="Исполнитель задачи")
     task_name: str = Field(description="Название задачи")
     created_at: datetime = Field(description="Дата и время создания задачи")
-    model_config = ConfigDict(from_attributes=True)
